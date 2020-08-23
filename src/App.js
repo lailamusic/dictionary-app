dc2bb7dcbbca525c4c4a83d97e25d54b18bc0b94
Laila Mušić <kesten.co.art@gmail.com>
2020-08-24 01:12:28 +0200

    Update 3
    Conditional loop if two equal words defined, user input stored in array (displayed in console log)

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import logo from './bookapp3.png'; import './App.css';
 //import Button from './app_component/Button';
 // *import AddNew from './app_component/AddNew';
 class App extends React.Component {
+
   constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {/*inBox1: " ", inBox2: " "*/};
       this.handleInputChange = this.handleInputChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleClick = this.handleClick.bind(this);
     }
-    handleInputChange(event) {
-      const target = event.target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-      const name = target.name;
 
-      this.setState({
-        [name]: value
-    });
+  handleInputChange(event) {
+      //this.setState({value: event.target.value});
+      //const target = event.target;
+    //  const value = target.value;
+    //  const name = target.name;
+      const wordGroup = [/*this.state.inBox1, this.state.inBox2*/];
+
+      //this.setState({
+      //  [name]: value
+    //});
    }
-    handleSubmit(event) {
-      event.preventDefault();
+
+  handleClick(event) {
+      var x = document.forms["appForm"]["inBox1"].value;
+      var y = document.forms["appForm"]["inBox2"].value;
+      var z = x + "-" + y + "/";
+      const wordGroup=[];
+      if (x === y && x !== "") {
+      alert("A word cannot be its own synonym."/* + this.state.value*/);
+      //event.preventDefault();
+      }
+      else {
+      wordGroup.push(z);
+      console.log(wordGroup);
+      }
+      // return <h1> Welcome.</h1>
+      //event.preventDefault();
     }
 
   render() {
@@ -29,17 +47,21 @@ class App extends React.Component {
         <p/>
         <img src={logo} className="App-logo" alt="logo"/>
         <p/>
-        <form onSubmit={this.handleSubmit} id="appForm">
+        <form onSubmit={this.handleSubmit} name="appForm" id="appForm">
           <input name="inBox1" type="text" className="inBox" placeholder=
           "add new word" autoComplete="off" value={this.state.inBox1} onChange={this.handleInputChange}/>
           <p/>
           <input name="inBox2" type="text" className="inBox" placeholder=
           "synonym" autoComplete="off" value={this.state.inBox2} onChange={this.handleInputChange}/>
+          <p/>
+        <button type="button" name="submit" className="enterWord" onClick={this.handleClick}/>
         </form>
-        <button type="submit" name="submit" className="enterWord"/>
 
+        <p>
         {this.state.inBox1 + " "}
         {this.state.inBox2 + " "}
+        </p>
+
         <p/>
       </header>
     </div>
--- src/app_component/newword.component.jsx
+++ src/app_component/newword.component.jsx
@@ -1,49 +1,13 @@
-import React from 'react'; import ReactDOM from 'react-dom';
-import logo from './bookapp3.png'; import './App.css';
-//import Button from './app_component/Button';
-// *import AddNew from './app_component/AddNew';
-class App extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        inBox1: undefined, inBox2: undefined
-      };
-      this.handleInputChange = this.handleInputChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleInputChange(event) {
-      const target = event.target;
-      //const value = target.type === 'checkbox' ? target.checked : target.value;
-      const name = target.name;
-
-      this.setState({
-        [name]: value
-    });
-   }
-    handleSubmit(event) {
-      event.preventDefault();
-    }
-
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p/>
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p/>
-        <form onSubmit={this.handleSubmit} id="appForm">
-          <input name="inBox1" type="text" className="inBox" placeholder=
-          "add new word" autoComplete="off" value={this.state.inBox1} onChange={this.handleInputChange}/>
-          <p/>
-          <input name="inBox2" type="text" className="inBox" placeholder=
-          "synonym" autoComplete="off" value={this.state.inBox2} onChange={this.handleInputChange}/>
-        </form>
-        <button type="submit" name="submit" className="enterWord"/>
-        <p/>
-      </header>
-    </div>
-  );
-}
-}
-
-export default App;
+handleClick:
+function (event) {
+     this.setState({ size: SizeEnum.SMALL });
+  },
+  render:
+  function() {
+            return (
+                <div>
+                    <div style = { this.style}></div>
+                    <input type="button" onClick={this.handleClick}
+                </div>
+            )
+  }
