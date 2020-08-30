0ee5b54adcac7874709a9fd9c0492ffe67318ace
Laila Mušić <kesten.co.art@gmail.com>
2020-08-30 23:52:36 +0200

    Update 5
    Developer edits: variable initialization, event states edited, array simplification

--- src/App.js
+++ src/App.js
@@ -5,64 +5,67 @@ import './App.css';
 // *import AddNew from './app_component/AddNew';
 class App extends React.Component {
 
-  constructor(props) {
-      super(props);
-      this.state = {/*inBox1: " ", inBox2: " "*/};
-      this.handleInputChange = this.handleInputChange.bind(this);
-      this.handleClick = this.handleClick.bind(this);
-      this.wordGroup = [];
-      this.dictionary = this.wordGroup.toString;
-    }
+constructor(props) {
+super(props);
+this.state = {
+text: '',
+word: '',
+synonym: '',
+dictionary: [],
+};
+}
 
-  handleInputChange(event) {
-   }
+handleWordChange = event => this.setState({ word: event.currentTarget.value });
 
-  handleClick(event) {
-      var x = document.forms["appForm"]["inBox1"].value;
-      var y = document.forms["appForm"]["inBox2"].value;
-      var z = x + "." + y;
-      if (x === y && x !== "") {
-      alert("A word cannot be its own synonym."/* + this.state.value*/);
-      //event.preventDefault();
-      }
-      else {
-        if (this.wordGroup.includes(x)===true) {
-          var k = this.wordGroup.indexOf(x);
-          var h = this.wordGroup[k];
-          this.wordGroup.splice(k,1, h+"."+y);
-        }
-      this.wordGroup.push(z);
+handleSynonymChange = event => this.setState({ synonym: event.currentTarget.value });
 
-      console.log(this.wordGroup);
-      document.getElementById('in1').value = "";
-      document.getElementById('in2').value = "";
-      }
-      //event.preventDefault();
-    }
+handleClick = event => {
+const { dictionary, word, synonym } = this.state;
 
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p/>
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p/> <p/>
-        <form onSubmit={this.handleSubmit} name="appForm" id="appForm">
-          <input name="inBox1" id="in1" type="text" className="inBox" placeholder=
-          "add new word" autoComplete="off" value={this.state.inBox1} onChange={this.handleInputChange}/>
-          <p/>
-          <input name="inBox2" id="in2" type="text" className="inBox" placeholder=
-          "synonym" autoComplete="off" value={this.state.inBox2} onChange={this.handleInputChange}/>
-          <p/>
-        <button type="button" name="submit" className="enterWord" onClick={this.handleClick}/>
-        </form>
-        <p/>
-        <h1>
-        </h1>
-        <p/>
-      </header>
-    </div>
-  );
+// TODO insert word/synonym into this.state.dictionary only if that is not already present
+
+
+dictionary.push([word, synonym]);
+this.setState({ dictionary });
+
+// var x = word;
+// var y = synonym;
+// var z = x + '.' + y;
+// if (x === y && x !== '') {
+// alert('A word cannot be its own synonym.'/* + this.state.value*/);
+// //event.preventDefault();
+// } else {
+// if (this.wordGroup.includes(x)) {
+// var k = this.wordGroup.indexOf(x);
+// var h = this.wordGroup[k];
+// this.wordGroup.splice(k, 1, h + '.' + y);
+// }
+// this.wordGroup.push(z);
+//
+// console.log(this.wordGroup);
+// this.setState({ text: this.wordGroup });
+// document.getElementById('in1').value = '';
+// document.getElementById('in2').value = '';
+// }
+//event.preventDefault();
+};
+
+render() {
+return (
+<div className="App">
+<div className="App-header">
+<img src={logo} className="App-logo" alt="logo"/>
+<input name="inBox1" id="in1" type="text" className="inBox" placeholder=
+"add new word" autoComplete="off" value={this.state.word} onChange={this.handleWordChange}/>
+<p/>
+<input name="inBox2" id="in2" type="text" className="inBox" placeholder=
+"synonym" autoComplete="off" value={this.state.synonym} onChange={this.handleSynonymChange}/>
+<p/>
+<button type="button" name="submit" className="enterWord" onClick={this.handleClick}/>
+</div>
+{JSON.stringify(this.state.dictionary)}
+</div>
+);
 }
 }
 
