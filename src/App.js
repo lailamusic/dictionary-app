fe58ec3a5f284e076adb22cedf7be87332388250
Laila Mušić <kesten.co.art@gmail.com>
2020-08-31 17:43:12 +0200

    Update 6
    Addition of CSS for second (right/search) block

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React from 'react';
 import logo from './bookapp3.png';
 import './App.css';
-//import Button from './app_component/Button';
-// *import AddNew from './app_component/AddNew';
 class App extends React.Component {
 
 constructor(props) {
 super(props);
-this.state = {
+this.state = { // initialization of variables
 text: '',
 word: '',
 synonym: '',
+find: '',
 dictionary: [],
+output: [],
 };
 }
 
 handleWordChange = event => this.setState({ word: event.currentTarget.value });
-
 handleSynonymChange = event => this.setState({ synonym: event.currentTarget.value });
+//handleFindChange = event => this.setState({ find: event.currentTarget.value });
 
 handleClick = event => {
-const { dictionary, word, synonym } = this.state;
-
+  const { dictionary, word, synonym } = this.state;
 // TODO insert word/synonym into this.state.dictionary only if that is not already present
-
-
 dictionary.push([word, synonym]);
 this.setState({ dictionary });
-
-// var x = word;
-// var y = synonym;
-// var z = x + '.' + y;
+//Lnotify.style.visibility = "visible";
 // if (x === y && x !== '') {
 // alert('A word cannot be its own synonym.'/* + this.state.value*/);
 // //event.preventDefault();
@@ -38,33 +32,51 @@ this.setState({ dictionary });
 // if (this.wordGroup.includes(x)) {
 // var k = this.wordGroup.indexOf(x);
 // var h = this.wordGroup[k];
-// this.wordGroup.splice(k, 1, h + '.' + y);
-// }
+// this.wordGroup.splice(k, 1, h + '.' + y);}
 // this.wordGroup.push(z);
-//
 // console.log(this.wordGroup);
 // this.setState({ text: this.wordGroup });
-// document.getElementById('in1').value = '';
-// document.getElementById('in2').value = '';
-// }
 //event.preventDefault();
 };
 
 render() {
+
 return (
-<div className="App">
-<div className="App-header">
-<img src={logo} className="App-logo" alt="logo"/>
-<input name="inBox1" id="in1" type="text" className="inBox" placeholder=
-"add new word" autoComplete="off" value={this.state.word} onChange={this.handleWordChange}/>
-<p/>
-<input name="inBox2" id="in2" type="text" className="inBox" placeholder=
-"synonym" autoComplete="off" value={this.state.synonym} onChange={this.handleSynonymChange}/>
-<p/>
-<button type="button" name="submit" className="enterWord" onClick={this.handleClick}/>
-</div>
-{JSON.stringify(this.state.dictionary)}
-</div>
+
+  <div className="App">
+
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo"/>
+    </div>
+
+    <div className="App-content">
+
+      <div className="App-Lblock">
+        <input name="inBox1" type="text" className=
+          "inBox-word" placeholder="add new word" autoComplete=
+          "off" value={this.state.word} onChange={this.handleWordChange}/>
+        <input name="inBox2" type="text" className=
+          "inBox-syn" placeholder="synonym" autoComplete=
+          "off" value={this.state.synonym} onChange={this.handleSynonymChange}/>
+        <button type="button" name="submit" className=
+          "enterWord" onClick={this.handleClick}/>
+        <div name="Lnotify" type="text" className=
+          "notify">Your definition has been added!</div>
+      </div>
+
+      <div className="App-Rblock">
+        <input name="findBox" type="text" className=
+          "inBox-find" placeholder="find a word" autoComplete=
+          "off" value={this.state.find} onChange={this.handleFindChange}/>
+        <button type="button" name="find" className=
+          "findWord" onClick={this.handleClick}/>
+        {JSON.stringify(this.state.dictionary)}
+      </div>
+
+    </div>
+
+  </div>
+
 );
 }
 }
--- src/app_component/newword.component.jsx
+++ src/app_component/newword.component.jsx
@@ -1,13 +1,72 @@
-handleClick:
-function (event) {
-     this.setState({ size: SizeEnum.SMALL });
-  },
-  render:
-  function() {
-            return (
-                <div>
-                    <div style = { this.style}></div>
-                    <input type="button" onClick={this.handleClick}
-                </div>
-            )
-  }
+import React from 'react';
+import logo from './bookapp3.png';
+import './App.css';
+//import Button from './app_component/Button';
+// *import AddNew from './app_component/AddNew';
+class App extends React.Component {
+
+constructor(props) {
+super(props);
+this.state = {
+text: '',
+word: '',
+synonym: '',
+dictionary: [],
+};
+}
+
+handleWordChange = event => this.setState({ word: event.currentTarget.value });
+
+handleSynonymChange = event => this.setState({ synonym: event.currentTarget.value });
+
+handleClick = event => {
+const { dictionary, word, synonym } = this.state;
+
+// TODO insert word/synonym into this.state.dictionary only if that is not already present
+
+
+dictionary.push([word, synonym]);
+this.setState({ dictionary });
+
+// var x = word;
+// var y = synonym;
+// var z = x + '.' + y;
+// if (x === y && x !== '') {
+// alert('A word cannot be its own synonym.'/* + this.state.value*/);
+// //event.preventDefault();
+// } else {
+// if (this.wordGroup.includes(x)) {
+// var k = this.wordGroup.indexOf(x);
+// var h = this.wordGroup[k];
+// this.wordGroup.splice(k, 1, h + '.' + y);
+// }
+// this.wordGroup.push(z);
+//
+// console.log(this.wordGroup);
+// this.setState({ text: this.wordGroup });
+// document.getElementById('in1').value = '';
+// document.getElementById('in2').value = '';
+// }
+//event.preventDefault();
+};
+
+render() {
+return (
+<div className="App">
+<div className="App-header">
+<img src={logo} className="App-logo" alt="logo"/>
+<input name="inBox1" id="in1" type="text" className="inBox" placeholder=
+"add new word" autoComplete="off" value={this.state.word} onChange={this.handleWordChange}/>
+<p/>
+<input name="inBox2" id="in2" type="text" className="inBox" placeholder=
+"synonym" autoComplete="off" value={this.state.synonym} onChange={this.handleSynonymChange}/>
+<p/>
+<button type="button" name="submit" className="enterWord" onClick={this.handleClick}/>
+</div>
+{JSON.stringify(this.state.dictionary)}
+</div>
+);
+}
+}
+
+export default App;
